6d5c77236aaf130bff2a3d21bbd4a77406480fe6
Dio Lemos <pedro.devsc@outlook.com>
2023-05-31 22:55:28 -0300

    create ui items labels, entries and buttons

--- main.py
+++ main.py
@@ -13,9 +13,28 @@ logo = PhotoImage(file="./images/logo.png")
 canvas = Canvas(width=200, height=200)
 main_image = canvas.create_image(100,100,image=logo)
 
-
 canvas.grid(row=0,column=1)
 
+#Labels
+website_label = Label(text="Website")
+
+email_label = Label(text="Email/Username:")
+
+password_label = Label(text="Password:")
+
+
+#Entries
+website_entry = Entry()
+
+email_entry = Entry()
+
+password_entry = Entry()
+
+#Buttons
+
+search_button = Button(text="Search")
+generate_button = Button(text="Generate")
+save_button = Button(text="Save")
 
 
 
