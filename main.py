101270dbf4147514be52d327aafc498296e8e141
Dio Lemos <pedro.devsc@outlook.com>
2023-05-31 22:59:30 -0300

    place labels using grid, change comments to be more  homosapiens friendly

--- main.py
+++ main.py
@@ -15,15 +15,20 @@ main_image = canvas.create_image(100,100,image=logo)
 
 canvas.grid(row=0,column=1)
 
-#Labels
+###-----Labels------###
+
 website_label = Label(text="Website")
+website_label.grid(column=0, row=1)
 
 email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
 
 password_label = Label(text="Password:")
+password_label.grid(column=0,row=3)
 
 
-#Entries
+###---Entries---###
+
 website_entry = Entry()
 
 email_entry = Entry()
@@ -32,7 +37,7 @@ password_entry = Entry()
 
 #Buttons
 
-search_button = Button(text="Search")
+search_button = Button(text="Search") 
 generate_button = Button(text="Generate")
 save_button = Button(text="Save")
 
